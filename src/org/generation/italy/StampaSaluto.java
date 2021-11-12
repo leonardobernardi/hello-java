1421bf2e0dc7607a7397fa814a649155253554f4
leonardobernardi <lbernardi4298@gmail.com>
2021-11-12 13:51:24 +0100

    commit

--- src/org/generation/italy/StampaSaluto.java
+++ src/org/generation/italy/StampaSaluto.java
@@ -1,10 +1,11 @@
 package org.generation.italy;
 
-public class HelloWorld {
+public class StampaSaluto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
+
 	}
 
 }
